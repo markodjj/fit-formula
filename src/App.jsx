ab3bae37a8c3c12360dee7c2eac3736210ef3dbe
markodjj <markodjordjevic244@gmail.com>
2024-12-25 20:59:50 +0100

    removing paragraph

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="bg-blue-500 text-white p-4 rounded-md shadow-md">
       <h1 className="text-2xl font-bold">Hello, Vite + React + Tailwind!</h1>
-      <p>Start building your project!</p>
     </div>
   );
 }
