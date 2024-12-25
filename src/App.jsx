58b83c64534153f7eb0e9f0e6296acb8f220a7cd
markodjj <markodjordjevic244@gmail.com>
2024-12-25 20:46:27 +0100

    new

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="bg-blue-500 text-white p-4 rounded-md shadow-md">
+      <h1 className="text-2xl font-bold">Hello, Vite + React + Tailwind!</h1>
+      <p>Start building your project!</p>
+    </div>
+  );
+}
+
+export default App;
+
